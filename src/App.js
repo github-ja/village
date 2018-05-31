3f20039a5d4981926c67fcc3dcf8eaa026e05812
Nicci Williams <nicci@nicciwill.com>
2018-05-31 08:26:28 -0400

    Create profile component (#1)

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Profile from './components/villager-profile';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1 className="intro">
+          Your Village
+        </h1>
+        <Profile
+          name='Anthony Jackson'
+          img='https://www.n2c0500.com/sites/default/files/styles/thumbnail/public/pictures/2018-03/Portrait-Male-Collared-Shirt-Placeholder-Image.jpg'
+        />
+        <Profile
+          name='Anne Williams'
+          img='https://s3.amazonaws.com/twenty28/sites/166676d6-e91b-11e4-8ed1-6a13df3711dd/headshots/2933/1523897780954/placeholder_female1-thumb.jpg'
+        />
+        <Profile
+          name='Craig Hansard'
+          img='https://www.n2c0500.com/sites/default/files/styles/thumbnail/public/pictures/2018-03/Portrait-Male-Collared-Shirt-Placeholder-Image.jpg'
+        />
       </div>
     );
   }
