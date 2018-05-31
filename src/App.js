ffc135c6ca05143935a094a6787c151a43af8ab2
github-ja <39779830+github-ja@users.noreply.github.com>
2018-05-31 08:45:31 -0400

    Added new profiler
    Added the profile for Pam Harris

--- src/App.js
+++ src/App.js
@@ -21,6 +21,10 @@ class App extends Component {
           name='Craig Hansard'
           img='https://www.n2c0500.com/sites/default/files/styles/thumbnail/public/pictures/2018-03/Portrait-Male-Collared-Shirt-Placeholder-Image.jpg'
         />
+        <Profile
+          name='Pam Harris'
+          img='https://s3.amazonaws.com/twenty28/sites/166676d6-e91b-11e4-8ed1-6a13df3711dd/headshots/2933/1523897780954/placeholder_female1-thumb.jpg'
+        />
       </div>
     );
   }
